7ac18be3d8acec40596764d496cbf2b80df54442
Jordan Rust <jordan@oxidalwave.com>
2023-08-01 08:44:23 -0500

    Update loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,18 +1,9 @@
 import HomePageContent from "./content";
 
-function defaultSeed() {
-  const date = new Date();
-  return new Date(
-    `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
-  ).getTime();
-}
-
 export default function HomePageLoading({
   searchParams,
 }: {
   searchParams: Record<string, never>;
 }) {
-  const seed = defaultSeed();
-
   return <HomePageContent dex={[]} />;
 }
