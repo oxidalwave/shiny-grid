3c7744a949a1eca67add7d4ca5f70c89bde9039c
Jordan Rust <jordan@oxidalwave.com>
2023-08-01 18:26:08 -0500

    Replace home page with redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,10 @@
 export const dynamic = "force-dynamic";
 
-import { use } from "react";
-import { getPokedex } from "~/lib/data/pokemon";
-import { getServerSession } from "next-auth";
-import App from "~/components/App";
 import { defaultSeed } from "~/lib/defaultSeed";
-import { z } from "zod";
-import { authOptions } from "~/server/auth";
-import { env } from "~/env.mjs";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
   const seed = defaultSeed();
 
-  const dex = use(getPokedex());
-
-  const session = use(getServerSession(authOptions));
-
-  const initialAnswers = session
-    ? use(
-        fetch(`${env.URL}/api/${seed}/${session.user.name}`)
-          .then((r) => r.json())
-          .then((j) =>
-            z
-              .array(
-                z.object({
-                  pokemonId: z.string(),
-                  categoryIndex: z.number(),
-                })
-              )
-              .parse(j)
-          )
-      )
-    : [];
-
-  return (
-    <div className="p-2">
-      <App username={session?.user?.name ?? undefined} dex={dex} seed={seed} initialAnswers={initialAnswers} />
-    </div>
-  );
+  redirect(`/${seed}`);
 }
