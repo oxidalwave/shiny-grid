bf20ed772017f49a122f27c677f710334dd5a897
Jordan Rust <jordan@oxidalwave.com>
2023-08-02 14:10:47 -0500

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import "~/styles/globals.css";
 import { type ReactNode } from "react";
 import Shell from "~/components/Shell";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <Shell>{children}</Shell>
+      <Analytics />
     </html>
   );
 }
