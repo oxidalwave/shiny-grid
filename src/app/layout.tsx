2eeb9324db85e70939ecf281efa8ea20d1a1d77f
Jordan Rust <jordan@oxidalwave.com>
2023-08-04 09:57:24 -0500

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,25 @@ import "~/styles/globals.css";
 import { type ReactNode } from "react";
 import Shell from "~/components/Shell";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
-      <Shell>{children}</Shell>
-      <Analytics />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Shiny Grid" />
+        <meta property="og:description" content="Game for guessing " />
+        <meta property="og:image" content="/icons/pokeball.png" />
+        <meta property="og:url" content="https://shiny-grid.oxidalwave.com" />
+      </Head>
+      <body className="bg-slate-900 font-pokemon text-white">
+        <Shell>{children}</Shell>
+        <Analytics />
+      </body>
     </html>
   );
 }
