52da0132b2a79633f6933891cb1ef67db469dafa
Jordan Rust <jordan@oxidalwave.com>
2023-08-07 08:56:55 -0500

    Remove Grid from loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,51 +1,9 @@
-export const revalidate = 21600;
-
-import { use } from "react";
-import { getServerSession } from "next-auth";
-import App from "~/components/App";
-import { authOptions } from "~/server/auth";
-import { defaultSeed } from "~/lib/defaultSeed";
-import { type Pokemon } from "~/lib/data/dex";
-import { getCategories } from "~/lib/getCategories";
 import Spinner from "~/components/common/Spinner";
-import { getCategoryFromId } from "~/lib/categories";
-import CategoryLabel from "~/components/CategoryLabel";
-import Header from "~/components/Header";
 
 export default function HomePageLoading() {
-  const seed = defaultSeed();
-
-  const dex = [] as Pokemon[];
-
-  const session = use(getServerSession(authOptions));
-
-  const initialAnswers = [] as {
-    categoryIndex: number;
-    pokemonId: string;
-  }[];
-
-  const categoryIds = getCategories(seed);
-  const categoryLabels = categoryIds
-    .map(getCategoryFromId)
-    .map((c) => <CategoryLabel key={c.id} category={c} />);
-
   return (
-    <>
-      <div className="h-screen flex items-center justify-center bg-slate-700 bg-opacity-50 z-10">
-        <Spinner />
-      </div>
-      <div className="p-2">
-        <App
-          categoryLabels={categoryLabels}
-          header={<Header seed={seed} />}
-          loading
-          categoryIds={categoryIds}
-          username={session?.user?.name ?? undefined}
-          dex={dex}
-          seed={seed}
-          initialAnswers={initialAnswers}
-        />
-      </div>
-    </>
+    <div className="h-screen flex items-center justify-center bg-slate-700 bg-opacity-50 z-10">
+      <Spinner />
+    </div>
   );
 }
