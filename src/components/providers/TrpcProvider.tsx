1280258251eca0c4484c6f41260eeed8509b9721
Jordan Rust <jordan@oxidalwave.com>
2024-02-07 09:26:46 -0600

    Fix trpcprovider

--- src/components/providers/TrpcProvider.tsx
+++ src/components/providers/TrpcProvider.tsx
@@ -36,9 +36,9 @@ export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({
               credentials: "include",
             });
           },
+          transformer: superjson,
         }),
       ],
-      transformer: superjson,
     }),
   );
   return (
